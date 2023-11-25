6dbdef5ae2282d56f53bd70448ddd13fa06a4237
Bekione <kinber024@gmail.com>
2023-11-25 14:42:44 +0300

    changed render method

--- app/services/ServiceHero.jsx
+++ app/services/ServiceHero.jsx
@@ -1,16 +1,12 @@
+import React from "react";
 import Image from "next/image";
 
 const ServiceHero = ({ Intro, imgPath, imgAlt }) => {
-    
   return (
     <div className="w-full flex flex-col flex-col-reverse md:flex-row justify-center items-center gap-5 px-10 mt-8">
       <div className="flex-1">
-        <p
-          className="mb-5 text-justify text-base font-normal leading-[1.5] text-body-dark sm:text-lg sm:leading-[1.5]"
-        >
-            {typeof Intro === "string" ? (
-                {Intro}
-            ) : (<Intro />) }
+        <p className="mb-5 text-justify text-base font-normal leading-[1.5] text-body-dark sm:text-lg sm:leading-[1.5]">
+          {typeof Intro === "string" ? Intro : React.createElement(Intro)}
         </p>
       </div>
       <div className="max-w-[85%] w-fit md:w-[350px] lg:[400px] xl:w-[450px]">
