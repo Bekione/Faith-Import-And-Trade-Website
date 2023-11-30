c4f72a8a9fd3ba08550efd245b0ca701de7be1dd
Bekione <kinber024@gmail.com>
2023-11-30 07:32:00 -0800

    avif to webp

--- components/Services/Stationary/itemsData.js
+++ components/Services/Stationary/itemsData.js
@@ -3,20 +3,20 @@ export const WritingInstruments = [
     id: "item001",
     itemName: "Pens",
     description: "Explore a variety of pens for all your writing needs.",
-    image: "/images/stationery/writing-1.avif",
+    image: "/images/stationery/writing-1.webp",
   },
   {
     id: "item002",
     itemName: "Pencils",
     description:
       "Find mechanical pencils, graphite pencils, and colored pencils.",
-    image: "/images/stationery/writing-2.avif",
+    image: "/images/stationery/writing-2.webp",
   },
   {
     id: "item003",
     itemName: "Markers and Highlighters",
     description: "Discover markers and highlighters in vibrant colors.",
-    image: "/images/stationery/writing-3.avif",
+    image: "/images/stationery/writing-3.webp",
   },
 ];
 
@@ -25,19 +25,19 @@ export const PaperProducts = [
     id: "item001",
     itemName: "Notebooks",
     description: "Choose from a variety of notebooks for capturing your ideas.",
-    image: "/images/stationery/paper-1.avif",
+    image: "/images/stationery/paper-1.webp",
   },
   {
     id: "item002",
     itemName: "Sticky Notes",
     description: "Find sticky notes in different sizes, shapes, and colors.",
-    image: "/images/stationery/paper-2.avif",
+    image: "/images/stationery/paper-2.webp",
   },
   {
     id: "item003",
     itemName: "Printing Papers",
     description: "Browse through memo pads for quick notes and reminders.",
-    image: "/images/stationery/paper-3.avif",
+    image: "/images/stationery/paper-3.webp",
   },
 ];
 
@@ -52,7 +52,7 @@ export const DeskOrganization = [
     id: "item002",
     itemName: "File Trays and Sorters",
     description: "Stay organized with file trays and letter sorters.",
-    image: "/images/stationery/desk-2.avif",
+    image: "/images/stationery/desk-2.webp",
   },
   {
     id: "item003",
@@ -67,19 +67,19 @@ export const ArtsCrafts = [
     id: "item001",
     itemName: "Paints and Brushes",
     description: "Unleash your creativity with paints and brushes.",
-    image: "/images/stationery/art-1.avif",
+    image: "/images/stationery/art-1.webp",
   },
   {
     id: "item002",
     itemName: "Sketchbooks",
     description: "Find sketchbooks for drawing and sketching.",
-    image: "/images/stationery/art-2.avif",
+    image: "/images/stationery/art-2.webp",
   },
   {
     id: "item003",
     itemName: "Craft Materials",
     description: "Discover a variety of craft materials for DIY projects.",
-    image: "/images/stationery/art-3.avif",
+    image: "/images/stationery/art-3.webp",
   },
 ];
 
@@ -89,19 +89,19 @@ export const Others = [
     itemName: "Box files",
     description:
       "Durable files for organizing and storing important documents.",
-    image: "/images/stationery/office-1.avif",
+    image: "/images/stationery/office-1.webp",
   },
   {
     id: "item002",
     itemName: "Staplers",
     description:
       "Various types of staplers for securely binding documents together.",
-    image: "/images/stationery/office-2.avif",
+    image: "/images/stationery/office-2.webp",
   },
   {
     id: "item003",
     itemName: "Many more",
     description: "And many more stationery equipments/machines you asked.",
-    image: "/images/stationery/office-3.avif",
+    image: "/images/stationery/office-3.webp",
   },
 ];
