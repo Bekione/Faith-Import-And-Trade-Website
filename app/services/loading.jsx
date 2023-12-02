2b3475f94ecef071b2920fc68cef7c6ec5877486
Bekione <kinber024@gmail.com>
2023-12-02 07:01:08 -0800

    added not-found page

---
+++ app/services/loading.jsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+    return(
+        <div className="h-screen w-full flex items-center justify-center bg-secondary/40 backdrop-blur-sm">
+            <div className="spinner_page"></div>
+        </div>
+        
+    )
+}
+
+export default Loading;
