4290a0e64beac863a1ec3d4d3e0982eee4b8827c
Bekione <kinber024@gmail.com>
2023-12-04 17:33:54 +0300

    changed image name

--- components/Customers/CustomersData.js
+++ components/Customers/CustomersData.js
@@ -1,52 +1,52 @@
 export const customersData = [
   {
     id: "img001",
-    path: "/images/customers/customer-1.webp",
+    path: "/images/customers/client-1.webp",
     title: "Global Green Growth Institute",
   },
   {
     id: "img002",
-    path: "/images/customers/customer-2.webp",
+    path: "/images/customers/client-2.webp",
     title: "Center for International Private Enterprise",
   },
   {
     id: "img003",
-    path: "/images/customers/customer-3.webp",
+    path: "/images/customers/client-3.webp",
     title: "GIZ",
   },
   {
     id: "img004",
-    path: "/images/customers/customer-4.webp",
+    path: "/images/customers/client-4.webp",
     title: "GOAL Global",
   },
   {
     id: "img005",
-    path: "/images/customers/customer-1.webp",
+    path: "/images/customers/client-1.webp",
     title: "Global Green Growth Institute",
   },
   {
     id: "img006",
-    path: "/images/customers/customer-2.webp",
+    path: "/images/customers/client-2.webp",
     title: "Center for International Private Enterprise",
   },
   {
     id: "img007",
-    path: "/images/customers/customer-3.webp",
+    path: "/images/customers/client-3.webp",
     title: "GIZ",
   },
   {
     id: "img008",
-    path: "/images/customers/customer-4.webp",
+    path: "/images/customers/client-4.webp",
     title: "GOAL Global",
   },
   {
     id: "img009",
-    path: "/images/customers/customer-1.webp",
+    path: "/images/customers/client-1.webp",
     title: "Global Green Growth Institute",
   },
   {
     id: "img010",
-    path: "/images/customers/customer-2.webp",
+    path: "/images/customers/client-2.webp",
     title: "Center for International Private Enterprise",
   },
 ];
